503bae0b14d8c64b85020f7f04784f224c4564c3
jasmin19 <jasmin.wycislok@nexiles.com>
2015-06-15 15:00:37 +0200

    extended code

--- Example_HelloWorld/app.js
+++ Example_HelloWorld/app.js
@@ -5,12 +5,33 @@ Ext.application({
 
        Ext.create('Ext.Panel', {
             renderTo     : Ext.getBody(),
-            width        : 200,
-            height       : 150,
+            width        : 600,
+            height       : 250,
             bodyPadding  : 5,
-            title        : 'Hello World',
+            title        : 'My first Ext JS Code',
             html         : 'Hello <b>World</b>...'
+    
         });
+
+       Ext.create('Ext.Button', {
+            text: 'Click me',
+            renderTo: Ext.getBody(),
+            width: 600,
+            handler: function() {
+                alert('You clicked the button!');
+            }
+        });
+
+       Ext.create('Ext.slider.Single', {
+            width: 600,
+            value: 0,
+            increment: 5,
+            minValue: -50,
+            maxValue: 50,
+            renderTo: Ext.getBody()
+});
+
 
     }
 });
+
